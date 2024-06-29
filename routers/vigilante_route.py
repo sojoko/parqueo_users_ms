e4ce6914519c6e91a89d0076886bb744363ab01d
Jonhathan Sojo <140211294+sojoko@users.noreply.github.com>
2024-06-29 02:11:23 -0500

    vigilant fix

--- routers/vigilante_route.py
+++ routers/vigilante_route.py
@@ -7,11 +7,7 @@ from models.vigilantes import Vigilantes as VigilanteModel
 from schemas.vigilantes import Vigilantes
 from models.aprendices import EstadoAprendiz
     
-
-
-
 vigilante_router = APIRouter()
-
 
 @vigilante_router.post("/api/v1/vigilantes-registration", tags=['Vigilantes'])
 async def create_vigilante(vigilantes: Vigilantes):
@@ -36,13 +32,11 @@ async def create_vigilante(vigilantes: Vigilantes):
 async def update_vigilante(document: int, vigilantes: Vigilantes):
     db = Session()
     try:
-        # Buscar el vigilante por el documento
         vigilante = db.query(VigilanteModel).filter(VigilanteModel.document == document).first()
         
         if not vigilante:
             raise HTTPException(status_code=404, detail="Vigilante no encontrado")
         
-        # Actualizar los datos del vigilante
         vigilante.name = vigilantes.name
         vigilante.last_name = vigilantes.last_name
         vigilante.document = int(vigilantes.document)
@@ -68,7 +62,7 @@ async def get_all_vigilant(page: int = Query(1, ge=1), per_page: int = Query(5, 
                 vigilantes_with_roll.append(user_dict)
             return JSONResponse(status_code=200, content=jsonable_encoder(vigilantes_with_roll))
         finally:
-            db.close()  # Cierra la sesión de SQLAlchemy después de usarla
+            db.close()
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error en la operación: {str(e)}")
 
